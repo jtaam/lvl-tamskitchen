7d278f161de631c7133b768efa6a0f626d7bdef5
Tam Jid <tamjid.mdh@gmail.com>
2018-09-26 23:34:36 +0600

    disable registration

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
 Route::group(['prefix'=>'admin','middleware'=>'auth','namespace'=>'admin'], function(){
     Route::get('dashboard','DashboardController@index')->name('admin.dashboard');
 });
