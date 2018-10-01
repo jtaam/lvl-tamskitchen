e4fb987fa744e162de7591fd558022112ca672ef
Tam Jid <tamjid.mdh@gmail.com>
2018-10-01 19:02:14 +0600

    notification email confirmed

--- app/Http/Controllers/admin/ReservationController.php
+++ app/Http/Controllers/admin/ReservationController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\admin;
 
+use App\Notifications\ReservationConfirmed;
 use App\Reservation;
 use Brian2694\Toastr\Facades\Toastr;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Notification;
 
 class ReservationController extends Controller
 {
@@ -18,6 +20,10 @@ class ReservationController extends Controller
         $reservation = Reservation::findOrFail($id);
         $reservation->status = true;
         $reservation->save();
+
+        Notification::route('mail', $reservation->email)
+            ->notify(new ReservationConfirmed());
+
 
         Toastr::success('Reservation confirmed succefully!','Done',["positionClass"=>"toast-top-right"]);
 
