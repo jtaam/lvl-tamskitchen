dad2268056babc35b0bbea1c717705d127ae8d30
_tam <tamjid84@yahoo.com>
2018-12-25 21:33:12 +0600

    slider background link fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
         @foreach($sliders as $key=>$slider)
         .owl-carousel .owl-wrapper, .owl-carousel .owl-item:nth-child({{$key + 1}}) .item
         {
-            background: url({{Storage::disk('/')->url('uploads/slider/'.$slider->image)}});
+            background: url({{Storage::disk('public')->url('uploads/slider/'.$slider->image)}});
             background-size: cover;
             /*background-position: bottom;*/
         }
